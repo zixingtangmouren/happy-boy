106db7c3add71285bda9c5cecf797dd3b365037e
唐志诚 <tangzhicheng@cvte.com>
2024-03-27 10:45:21 +0800

    feat: 支持 less

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
-  return <div>Hi, Happy boy 哈哈哈终于成功了</div>;
+  return (
+    <div>
+      教研员平台
+      <BrowserRouter basename="/">
+        <Link to="/">首页</Link>
+        <Link to="/about">教研活动</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div>About</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import styles from './index.module.less';
+
+function Home() {
+  return <div className={styles.home}>教研员平台</div>;
+}
+
+export default Home;
