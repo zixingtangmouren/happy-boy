cb21333dd3ac1cd869259c7c9bc47c26b6554609
唐志诚 <tangzhicheng@cvte.com>
2024-03-28 17:09:21 +0800

    feat: 补充 assets 资源

--- server/app/controller/home.ts
+++ server/app/controller/home.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default class HomeController extends Controller {
   public async index() {
+    const assets = this.ctx.getAssets();
     if (this.app.config.env === 'local') {
       const server = await this.ctx.app.viteServer!;
       // 使用vite服务输出视图
@@ -15,13 +16,13 @@ export default class HomeController extends Controller {
         )
       );
 
-      const assets = this.ctx.getAssets();
-
       this.ctx.body = await this.ctx.renderString(html, {
         ...assets,
       });
     } else {
-      await this.ctx.render('index.html');
+      await this.ctx.render('index.html', {
+        ...assets,
+      });
     }
   }
 }
